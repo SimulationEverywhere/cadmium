426087bec50ec40fe5184f5167e94ba67d23bd7c
Damian Vicino <sdavtaker@users.noreply.github.com>
2017-03-27 18:26:04 -0400

    basic logger support

---
+++ include/cadmium/logger/logger.hpp
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2013-2017, Damian Vicino
+ * Carleton University, Universite de Nice-Sophia Antipolis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <sstream>
+
+/**
+  * Logging concepts
+  * A logger source is where information of potential interest is provided to the logger.
+  *   Syntax is similar to IOstream using operator<<.
+  *   In addition to the information to log, a set of identifiers is provided to decide where to route it.
+  * A filter makes decisions, based in the set of identifiers provided by the log source, to effectively log the information.
+  *   We filter at compile time to reduce any undecired logging overhead.
+  * A formatter transforms the information that was not filtered out into a log record.
+  * A sink provides the way to move formated log records to its final destination.
+  *
+  * Expected usage
+  *   This logger is not a generic implementation to use in any system. In the contrary, it tries to exploit
+  *   particularities of Cadmium simulator to reduce overhead.
+  *
+  * Assumptions at this stage
+  *   All log sources are well-known (runner, coordinator, simulator), and there is not point of extension.
+  *   Each introduction of log source information, if not filtered out, will produce one log record.
+  *   All possible identifier are known.
+  *
+  * Future additions
+  *   We will like to allow an extension for model specific log sources and filters.
+  */
+
+//log sources are identified as childs of this class
+struct logger_source{};
+
+//formatter for verbatim concatenated outputs
+template<typename... PARAMs>
+struct verbatim_formater;
+
+template<typename PARAM, typename... PARAMs>
+struct verbatim_formater<PARAM, PARAMs...>{
+    static void format(std::ostream& os, const PARAM& p, const PARAMs&... ps){
+        os << p;
+        verbatim_formater<PARAMs...>::format(os, ps...);
+    }
+};
+
+template<>
+struct verbatim_formater<>{
+    static void format(std::ostream& os){
+        os << std::endl;
+    }
+};
+
+
+
+template<typename LOGGER_SOURCE, template<typename...> class FORMATTER>
+struct logger{
+    logger() = delete;
+
+    std::ostream& _sink; //todo: try to remove need for pointer
+    logger(std::ostream& s): _sink(s){} //sets the sink where data will be routed. Sink will receive streams to publish
+
+
+    template<typename DECLARED_SOURCE, typename... PARAMs>
+    void log(const PARAMs&... ps){ //todo: apply a static_if solution
+        if (std::is_same<LOGGER_SOURCE, DECLARED_SOURCE>::value) {
+            FORMATTER<PARAMs...>::format(_sink, ps...);
+        }
+    }
+};
+
+
+
+
+#endif // LOGGER_HPP
