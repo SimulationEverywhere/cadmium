0cd5460240706ce07b5cd62f92ba8212e337fef3
Laouen <laouen.belloli@gmail.com>
2017-12-16 23:54:19 -0300

    Added copyrights note to dynamic_atomic files

--- test/pdevs_dynamic_atomic_test.cpp
+++ test/pdevs_dynamic_atomic_test.cpp
@@ -24,15 +24,25 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef CADMIUM_ATOMIC_HPP
-#define CADMIUM_ATOMIC_HPP
 
-namespace cadmium {
-namespace modeling {
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <cadmium/basic_model/accumulator.hpp>
+#include <cadmium/modeling/dynamic_atomic.hpp>
 
-class atomic { };
+/**
+  * This test is for the dynamic atomic class that wraps an atomic model to make it pointer friendly
+  */
+template<typename TIME>
+using int_accumulator=cadmium::basic_models::accumulator<int, TIME>;
 
-}
-}
+BOOST_AUTO_TEST_SUITE( pdevs_dynamic_atomic_test_suite )
 
-#endif // CADMIUM_ATOMIC_HPP
+    BOOST_AUTO_TEST_CASE(create_dynamic_atomic_test) {
+        int_accumulator<float> model;
+        cadmium::modeling::dynamic_atomic<float, int_accumulator> wrapped_model;
+        BOOST_CHECK(typeid(model.state) == typeid(wrapped_model.state));
+        BOOST_CHECK(model.state == wrapped_model.state);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
