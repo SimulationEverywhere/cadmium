756aa2288815acbacffc767fa467c97eaf2e0a94
Laouen <laouen.belloli@gmail.com>
2018-01-10 09:57:58 -0300

    moved for_each helper function to cadmium/engine/common_helpers file to be used by both the original and dynamic cadmium

---
+++ include/cadmium/engine/common_helpers.hpp
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2018, Laouen M. L. Belloli
+ * Carleton University, Universidad de Buenos Aires
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CADMIUM_COMMON_HELPERS_HPP
+#define CADMIUM_COMMON_HELPERS_HPP
+
+#include <tuple>
+
+namespace cadmium {
+    namespace helper {
+
+        //Generic tuple for_each function
+        template<typename TUPLE, typename FUNC>
+        void for_each(TUPLE& ts, FUNC&& f) {
+
+            auto for_each_fold_expression = [&f](auto &... e)->void { (f(e) , ...); };
+            std::apply(for_each_fold_expression, ts);
+        }
+
+    }
+}
+
+#endif //CADMIUM_COMMON_HELPERS_HPP
