41cfef09a47334d8523490fea1725cba6ca21f9a
Laouen <laouen.belloli@gmail.com>
2018-01-31 13:17:00 -0300

    BugFix in the coordinator EIC, EOC, IC structre mapping from the model structures

--- include/cadmium/engine/pdevs_dynamic_coordinator.hpp
+++ include/cadmium/engine/pdevs_dynamic_coordinator.hpp
@@ -93,21 +93,27 @@ namespace cadmium {
                     }
 
                     // Generates structures for direct access to external couplings to not iterate all coordinators each time.
+                    bool found;
                     for (const auto& eoc : coupled_model->_eoc) {
                         if (enginges_by_id.find(eoc._from) == enginges_by_id.end()) {
                             throw std::domain_error("External output coupling from invalid model");
                         }
 
+                        found = false;
                         for (auto& coupling : _external_output_couplings) {
                             if (coupling.first->get_model_id() == eoc._from) {
                                 coupling.second.push_back(eoc._link);
-                                continue;
+                                found = true;
+                                break;
                             }
                         }
-                        cadmium::dynamic::engine::external_coupling<TIME> new_eoc;
-                        new_eoc.first = enginges_by_id.at(eoc._from);
-                        new_eoc.second.push_back(eoc._link);
-                        _external_output_couplings.push_back(new_eoc);
+
+                        if (!found) {
+                            cadmium::dynamic::engine::external_coupling<TIME> new_eoc;
+                            new_eoc.first = enginges_by_id.at(eoc._from);
+                            new_eoc.second.push_back(eoc._link);
+                            _external_output_couplings.push_back(new_eoc);
+                        }
                     }
 
                     for (const auto& eic : coupled_model->_eic) {
@@ -115,16 +121,21 @@ namespace cadmium {
                             throw std::domain_error("External input coupling to invalid model");
                         }
 
+                        found = false;
                         for (auto& coupling : _external_input_couplings) {
                             if (coupling.first->get_model_id() == eic._to) {
                                 coupling.second.push_back(eic._link);
-                                continue;
+                                found = true;
+                                break;
                             }
                         }
-                        cadmium::dynamic::engine::external_coupling<TIME> new_eic;
-                        new_eic.first = enginges_by_id.at(eic._to);
-                        new_eic.second.push_back(eic._link);
-                        _external_input_couplings.push_back(new_eic);
+
+                        if (!found) {
+                            cadmium::dynamic::engine::external_coupling<TIME> new_eic;
+                            new_eic.first = enginges_by_id.at(eic._to);
+                            new_eic.second.push_back(eic._link);
+                            _external_input_couplings.push_back(new_eic);
+                        }
                     }
 
                     for (const auto& ic : coupled_model->_ic) {
@@ -132,17 +143,22 @@ namespace cadmium {
                             throw std::domain_error("Internal coupling to invalid model");
                         }
 
+                        found = false;
                         for (auto& coupling : _internal_coupligns) {
                             if (coupling.first.first->get_model_id() == ic._from && coupling.first.second->get_model_id() == ic._to) {
                                 coupling.second.push_back(ic._link);
-                                continue;
+                                found = true;
+                                break;
                             }
                         }
-                        cadmium::dynamic::engine::internal_coupling<TIME> new_ic;
-                        new_ic.first.first = enginges_by_id.at(ic._from);
-                        new_ic.first.second = enginges_by_id.at(ic._to);
-                        new_ic.second.push_back(ic._link);
-                        _internal_coupligns.push_back(new_ic);
+
+                        if(!found) {
+                            cadmium::dynamic::engine::internal_coupling<TIME> new_ic;
+                            new_ic.first.first = enginges_by_id.at(ic._from);
+                            new_ic.first.second = enginges_by_id.at(ic._to);
+                            new_ic.second.push_back(ic._link);
+                            _internal_coupligns.push_back(new_ic);
+                        }
                     }
                 }
 
